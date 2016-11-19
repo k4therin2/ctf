b613aff3571d075d4239a279238d1339bb475189
Adam Johnston <amj69@case.edu>
2016-11-19 04:27:53 -0500

    initial commit of flask ctf server

---
+++ server/server.py
@@ -0,0 +1,83 @@
+from flask import Flask
+import base64
+import hashlib
+import shelve
+
+
+scores_dict = None
+
+
+# Add flags here mapping to their number in our list. For sure a better way
+# to go about this, but this is quick and dirty. Also should we standardize
+# flag format lol?
+flag_map = {
+    'flag{gary-ignatius-teabody}': 12,
+    'pan galactic gargle blaster': 21,
+}
+
+
+def get_salted_hash(username, actual_flag):
+    sha = hashlib.sha1()
+    sha.update(bytes(username + ':' + actual_flag, 'ascii'))
+    return base64.b16encode(sha.digest())
+
+
+def handle_flag_submit(username, flag=None):
+    completed = scores_dict[username] if username in scores_dict else set()
+
+    response = 'hi {}<br><br>'.format(username)
+
+    if flag:
+        flag = bytes(flag.upper(), 'ascii')
+        for actual_flag in flag_map:
+            print(flag)
+            print(get_salted_hash(username, actual_flag))
+            if flag == get_salted_hash(username, actual_flag):
+                completed.add(flag_map[actual_flag])
+                scores_dict[username] = completed
+
+                print('{} completed {}'.format(username, flag_map[actual_flag]))
+
+    if len(completed) > 0:
+        response += 'completed: {}'.format(', '.join(map(str, completed)))
+    else:
+        response += 'you haven\'t found anything yet :('
+
+    return response
+
+
+def make_leaderboards():
+    board = list(map(lambda x: (x[0], len(x[1])), scores_dict.items()))
+    board.sort(key=lambda x: -x[1])
+
+    i = 1
+    last_place = 0
+    last_score = 100
+
+    leaderboards = '<br><br><br><strong>Leaderboards</strong><br>'
+
+    for user in board:
+        if user[1] != last_score:
+            last_place = i
+        leaderboards += '{}. {}<br>'.format(last_place, user[0])
+        i += 1
+        last_score = user[1]
+
+    return leaderboards
+
+
+if __name__ == '__main__':
+    scores_dict = shelve.open('scores')
+
+    with open('index.html') as f:
+        index = f.read()
+
+    app = Flask('ctf_leaderboards')
+
+    app.route('/')(lambda: index + make_leaderboards())
+
+    app.route('/<string:username>')(handle_flag_submit)
+    app.route('/<string:username>/')(handle_flag_submit)
+    app.route('/<string:username>/<string:flag>')(handle_flag_submit)
+
+    app.run()
