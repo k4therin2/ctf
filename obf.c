f4dadc4047eb587cf7465443d5af93ab9f779a8e
Stephen Brennan <smb196@case.edu>
2016-12-03 12:43:37 -0500

    Add obfuscated code hint.

--- obf.c
+++ obf.c
@@ -1,2 +1,4 @@
-#include <stdio.h>
-char turing[] = "\033\027\002\032\000\131\015\024\014\015\036\016\022\021\012\107\006";char semaphore = '}';char trie = '{';char link_state = 'c';int main(int argc, char **argv) {  int i;  for (i = 0; i < sizeof(turing) - 1; i++) {    if (i % 3 == 0) {      fputc(turing[i] ^ semaphore, stdout);    } else if (i % 3 == 1) {      fputc(turing[i] ^ trie, stdout);    } else {      fputc(turing[i] ^ link_state, stdout);    }  }  fputc('\n', stdout);}
+char turing[]="\033\027\002\032\000\131\015\024\014\015\036\016\022\021\012\107\006";
+char semaphore='}';char trie='{';char link_state='c';int main(int argc,char **argv){
+int i;for(i=0;i<sizeof(turing)-1;i++){if(i%3==0){putchar(turing[i]^semaphore);}else
+if(i%3==1){putchar(turing[i]^trie);}else{putchar(turing[i]^link_state);}}putchar('\n');}
