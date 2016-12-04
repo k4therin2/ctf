53852a7b5534ce9b70200832c0a8ecca309b6af2
Matthew Bentley <matthew@bentley.link>
2016-12-04 14:13:36 -0500

    Update the repo to reflect reality

--- tcpdumpflag.rs
+++ tcpdumpflag.rs
@@ -1,8 +1,20 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Write;
+use std::io::{Read, Write};
+use std::time::Duration;
 
 
 fn handle_client(mut stream: TcpStream) {
+    let _ = stream.set_read_timeout(Some(Duration::new(1, 0)));
+    let mut b: [u8; 16] = [0; 16];
+    let r = stream.read(&mut b);
+    match r {
+        Ok(n) => {
+            if n != 0 {
+                return;
+            }
+        }
+        Err(_) => {}
+    }
     let response = "flag{All men are created equal}\n";
     let _ = stream.write(response.as_bytes());
 }
